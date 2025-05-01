6b36514b7a07ab8e8447f6f988c79e163f46ac0a
Sam Weiss <sweiss@cct.ie>
2025-05-01 13:21:49 +0100

    Dead and live locks 1

--- app.js
+++ app.js
@@ -19,37 +19,87 @@ const Account = mongoose.model('Account', accountSchema);
 // If the account exists, set the balance to 0;
 Account.findById(1)
 .then(account => {
-    if (account) account.balance = 0;
-    else account = new Account({ _id: 1, balance: 0 });
+    if (account) account.balance = 1000;
+    else account = new Account({ _id: 1, balance: 1000 });
+    account.save();
+    console.log(account);
+});
+
+Account.findById(2)
+.then(account => {
+    if (account) account.balance = 1000;
+    else account = new Account({ _id: 2, balance: 1000 });
     account.save();
     console.log(account);
 });
 
 // Function to simulate a race condition by directly manipulating account balance without synchronization
-async function handleDeposit(amount) {
+async function handleDeposit(amount, accountNum) {
     await accountMutex.runExclusive(async () => {
         console.log(`Depositing $${amount}`);
-        let account = await Account.findOne({ "_id": 1 });
+        let account = await Account.findOne({ "_id": accountNum });
         account.balance += amount;
         console.log(account.balance);
         account.save();
     });
 }
 
-async function handleWithdrawal(amount) {
-    console.log(`Withdrawing $${amount}`);
-    let account = await Account.findOne({ "_id": 1 });
-    account.balance -= amount;
-    console.log(account.balance);
-    account.save();
+async function handleDeposit2(amount, accountNum) {
+    // console.log(accountMutex.isLocked())
+    const release = await accountMutex.acquire();
+    // console.log(accountMutex.isLocked())
+    // release();
+    // console.log(accountMutex.isLocked())
+    // const release2 = await accountMutex.acquire();
+    try {
+        console.log(`Depositing $${amount}`);
+        let account = await Account.findOne({ "_id": accountNum });
+        account.balance += amount;
+        console.log(account.balance);
+        account.save();
+        throw Error;
+    } catch (error) {
+        console.log(error);
+        throw Error;
+    } finally {
+        release();
+        console.log("Finally")
+    }
+    console.log("After")
+    release();
+}
+
+async function handleWithdrawal(amount, accountNum) {
+    await accountMutex.runExclusive(async () => {
+        console.log(`Withdrawing $${amount}`);
+        let account = await Account.findOne({ "_id": accountNum });
+        account.balance -= amount;
+        console.log(account.balance);
+        account.save();
+    });
+}
+
+async function handleTransfer(amount, sender, recipiant) {
+    await accountMutex.runExclusive(async () => {
+        await handleWithdrawal(amount, sender);
+        for (let i=0; i<1000000000; i++) {
+            i*i;
+        }
+        await handleDeposit(amount, recipiant)
+        // setTimeout(async () => {
+            
+        // }, 5000)
+        // handleDeposit(amount, recipiant);
+    });
 }
 
 // Deposit route
 // eg http://localhost:3000/deposit?amount=100
 app.get('/deposit', async (req, res) => {
     const amount = parseInt(req.query.amount, 10) || 0;
+    const accountNum = parseInt(req.query.id, 10) || 0;
     // Simulate a race condition by directly modifying the balance without synchronization
-    handleDeposit(amount);
+    handleDeposit2(amount, accountNum);
     res.send(`Deposited $${amount}.`);
 });
 
@@ -60,6 +110,14 @@ app.get('/withdraw', async (req, res) => {
     handleWithdrawal(amount);
     res.send(`Withdrew $${amount}`);
 });
+
+app.get("/transfer", async (req, res) => {
+    const amount = parseInt(req.query.amount, 10) || 0;
+    const sender = parseInt(req.query.sender, 10) || 0;
+    const recipiant = parseInt(req.query.recipiant, 10) || 0;
+    handleTransfer(amount, sender, recipiant);
+    res.send("Success!");
+})
 
 app.get('/balance', async (req, res) => {
     let account = await Account.findOne({ "_id": 1 });
